93bbde99c0a5bfe368f3941b5cf72f95f6c0df87
TorresLino <arthur.torres.de.lino@gmail.com>
2023-07-20 09:10:13 -0600

    reorganizing project structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Providers from './providers'
+import { Inter, Noto_Sans_JP } from 'next/font/google'
+import Providers from '../components/Providers'
 
 const inter = Inter({ subsets: ['latin'] })
+const noto = Noto_Sans_JP({ subsets: ['latin']})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -17,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={noto.className}>
         <Providers>
           {children}
         </Providers>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import { HeaderSimple } from "./HeaderSimple";
+import { HeaderSimple } from "../components/HeaderSimple";
 
 export default function Home() {
   return (
     <>
       <HeaderSimple links={[{label: 'test1', link: '/'}]}/>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        TEST
+        綺麗なウェブサイトですね
       </main>
     </>
   )
