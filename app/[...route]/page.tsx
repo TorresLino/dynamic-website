50a102605dd859613cd2417ebb13232c6b11edce
TorresLino <arthur.torres.de.lino@gmail.com>
2023-11-09 01:32:22 -0700

    routing changes for editing and editor enhancement

--- app/[...route]/page.tsx
+++ app/[...route]/page.tsx
@@ -1,9 +1,43 @@
-import PageHtmlParser from '@/components/PageHtmlParser'
+import ArticleContents, { ArticleContent } from '@/models/ArticleContents'
+import Articles, { Article } from '@/models/Articles'
+import PageEditSwitch from '@/components/PageEditSwitch'
+import connectMongo from '@/lib/mongoose'
+import { notFound } from 'next/navigation'
 
-export default function Page({ params: { route } }: { params: { route: string[] } }) {
-    //TODO: determine how to store/determine if the user is in edit mode
-    //TODO: check if the user is editing tha page and return the editable layout
-    return (
-        <PageHtmlParser route={route} />
-    )
+export default async function Page({ params: { route } }: { params: { route: string[] } }) {
+    await connectMongo()
+
+    const articles = await Articles.find()
+
+    const getArticle = (route: string[], articles: Article[]): Article | undefined => {
+        const path = route.shift()
+
+        if (!path) {
+            return undefined
+        }
+
+        const article = articles.find(a => a.path === path)
+
+        if (!article) {
+            return undefined
+        }
+
+        return route.length === 0
+            ? article
+            : getArticle(route, article.childArticles)
+    }
+
+    const article = getArticle(route, articles)
+
+    if (!article) {
+        notFound()
+    }
+
+    const content = await ArticleContents.findById(article.contentId) as ArticleContent
+
+    if (!content) {
+        notFound()
+    }
+
+    return <PageEditSwitch content={content.content} />
 }
--- components/PageHtmlParser.tsx
+++ components/PageHtmlParser.tsx
@@ -1,55 +1,12 @@
-'use server'
-import ArticleContents, { ArticleContent } from '@/models/ArticleContents'
-import Articles, { Article } from '@/models/Articles'
-import connectMongo from '@/lib/mongoose'
-import { notFound } from 'next/navigation'
+'use client'
 import parse from 'html-react-parser'
 
 export interface PageHtmlParserProps {
-    route: string[]
+    content: string
 }
 
-interface ArticlePathQuery {
-    'childArticles.path'?: ArticlePathQuery
-    path: string
-}
+export default function Page(props: PageHtmlParserProps) {
+    const { content } = props
 
-export default async function Page(props: PageHtmlParserProps) {
-    const { route } = props
-
-    await connectMongo()
-
-    const articles = await Articles.find()
-
-    const getArticle = (route: string[], articles: Article[]): Article | undefined => {
-        const path = route.shift()
-
-        if (!path) {
-            return undefined
-        }
-
-        const article = articles.find(a => a.path === path)
-
-        if (!article) {
-            return undefined
-        }
-
-        return route.length === 0
-            ? article
-            : getArticle(route, article.childArticles)
-    }
-
-    const article = getArticle(route, articles)
-
-    if (!article) {
-        notFound()
-    }
-
-    const content = await ArticleContents.findById(article.contentId) as ArticleContent
-
-    if (!content) {
-        notFound()
-    }
-
-    return parse(content.content)
+    return parse(content)
 }
