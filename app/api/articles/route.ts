367b3369f0da5368ade6a9cb6bd84d62ce3b0223
TorresLino <arthur.torres.de.lino@gmail.com>
2024-01-16 00:04:35 -0700

    change Response to NextResponse

--- app/api/articles/route.ts
+++ app/api/articles/route.ts
@@ -1,9 +1,10 @@
 import Articles, { Article } from '@/models/Articles'
+import { NextResponse } from 'next/server'
 import connectMongo from '@/lib/mongoose'
 
 export async function GET(request: Request) {
     await connectMongo()
     const articles = await Articles.find() as Article[]
 
-    return Response.json({ articles })
+    return NextResponse.json({ articles })
 }
